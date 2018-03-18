0be2d70dfb76e039d0b81faf9f3131ea7eea9a09
Martynas Sapoka <martynas.sapoka@tradimo.com>
2018-03-18 21:02:33 +0100

    Remove state mutations

--- src/components/ClientTable/ClientTableContainer.js
+++ src/components/ClientTable/ClientTableContainer.js
@@ -17,14 +17,7 @@ class ClientsTableContainer extends React.Component {
 
 const mapStateToProps = state => {
   return {
-    customers: state.customers.map(c =>  {
-      c.totalHours = c.billings.reduce((total, b) => total + b.hours, 0);
-      c.billings.map(b => {
-        b.total = b.hours * b.hourlyRate;
-        return b;
-      });
-      return c;
-    })
+    customers: state.customers
   }
 };
 
--- src/components/CustomerRow/CustomerRow.js
+++ src/components/CustomerRow/CustomerRow.js
@@ -6,8 +6,8 @@ import {Link} from "react-router-dom";
 const CustomerRow = ({customer}) => (
   <tr className="JobRow">
     <td><Link to={`customers/${customer._id}`}>{customer.name}</Link></td>
-    <td>{customer.totalHours}</td>
-    <td>€{customer.billings.reduce((total, b) => total + b.total, 0)}</td>
+    <td>{customer.billings.reduce((total, b) => total + b.hours, 0)}</td>
+    <td>€{customer.billings.map(b => b.hours * b.hourlyRate).reduce((total, b) => total + b, 0)}</td>
   </tr>
 );
 
